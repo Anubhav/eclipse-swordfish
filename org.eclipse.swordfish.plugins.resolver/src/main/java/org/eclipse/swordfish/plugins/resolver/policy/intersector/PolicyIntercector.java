6285038ebfb27080011bff2ee1023bb82067c7cb
vzhabiuk <vzhabiuk@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-03-18 15:51:51 +0000

    Removed some classes from plugins resolver project
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.swordfish/trunk@20667 ee007c2a-0a25-0410-9ab9-bf268980928c

--- org.eclipse.swordfish.plugins.resolver/src/main/java/org/eclipse/swordfish/plugins/resolver/policy/intersector/PolicyIntercector.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.swordfish.plugins.resolver.policy.intersector;
-
-import org.apache.neethi.Policy;
-
-public interface PolicyIntercector {
-    public Policy intersect(Policy firstPolicy, Policy secondPolicy);
-}
