da502cb6c8ce51c555c4257e09d3800237fc9a57
vzhabiuk <vzhabiuk@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-03-19 11:36:33 +0000

    corrected code style
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.swordfish/trunk@20677 ee007c2a-0a25-0410-9ab9-bf268980928c

--- org.eclipse.swordfish.samples/org.eclipse.swordfish.samples.cxfendpoint/src/main/java/org/eclipse/swordfish/samples/cxf/domain/Flight.java
+++ org.eclipse.swordfish.samples/org.eclipse.swordfish.samples.cxfendpoint/src/main/java/org/eclipse/swordfish/samples/cxf/domain/Flight.java
@@ -4,32 +4,40 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     SOPERA GmbH - initial API and implementation
  *******************************************************************************/
 package org.eclipse.swordfish.samples.cxf.domain;
 
 public class Flight {
+
+	private int id;
+	private String flightNumber;
+
 	public Flight() {
 		// TODO Auto-generated constructor stub
 	}
+
+	public Flight(int id, String flightNumber) {
+		this.id = id;
+		this.flightNumber = flightNumber;
+	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getFlightNumber() {
 		return flightNumber;
 	}
+
 	public void setFlightNumber(String flightNumber) {
 		this.flightNumber = flightNumber;
 	}
-	private int id;
-	private String flightNumber;
-public Flight(int id, String flightNumber) {
-	this.id = id;
-	this.flightNumber = flightNumber;
-}
+
 }
