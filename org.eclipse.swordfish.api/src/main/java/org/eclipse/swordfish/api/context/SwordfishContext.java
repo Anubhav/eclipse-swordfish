22ca8dbc7a8e7f8f52f2b9c9c83812b097ad8d2e
vzhabiuk <vzhabiuk@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-03-23 10:31:34 +0000

    Removed endpointRegistry method
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.swordfish/trunk@20714 ee007c2a-0a25-0410-9ab9-bf268980928c

--- org.eclipse.swordfish.api/src/main/java/org/eclipse/swordfish/api/context/SwordfishContext.java
+++ org.eclipse.swordfish.api/src/main/java/org/eclipse/swordfish/api/context/SwordfishContext.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     SOPERA GmbH - initial API and implementation
  *******************************************************************************/
@@ -13,7 +13,6 @@ package org.eclipse.swordfish.api.context;
 
 import javax.jbi.component.ComponentContext;
 
-import org.apache.servicemix.nmr.api.EndpointRegistry;
 import org.eclipse.swordfish.api.configuration.ConfigurationService;
 import org.eclipse.swordfish.api.event.EventService;
 
@@ -25,13 +24,6 @@ import org.eclipse.swordfish.api.event.EventService;
  */
 public interface SwordfishContext {
 
-	/**
-     * Access the SMX endpoint registry.
-     * @return the ServiceMix platform endpoing registry.
-     */
-    EndpointRegistry getEndpointRegistry();
-
-
     ConfigurationService getConfigurationService();
 
     /**
@@ -39,7 +31,7 @@ public interface SwordfishContext {
      * @return the platform JBI context.
      */
     ComponentContext getComponentContext();
-    
+
     /**
      * Access to Swordfish EventService
 	 */
