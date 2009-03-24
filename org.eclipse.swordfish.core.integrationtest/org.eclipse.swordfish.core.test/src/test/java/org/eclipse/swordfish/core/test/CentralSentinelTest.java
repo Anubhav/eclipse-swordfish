c444b0eab89457051f76aed7fe305ce9709f3311
vzhabiuk <vzhabiuk@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-03-24 09:51:48 +0000

    Added Central Sentinel Test
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.swordfish/trunk@20720 ee007c2a-0a25-0410-9ab9-bf268980928c

--- org.eclipse.swordfish.core.integrationtest/org.eclipse.swordfish.core.test/src/test/java/org/eclipse/swordfish/core/test/CentralSentinelTest.java
+++ org.eclipse.swordfish.core.integrationtest/org.eclipse.swordfish.core.test/src/test/java/org/eclipse/swordfish/core/test/CentralSentinelTest.java
@@ -3,6 +3,8 @@ package org.eclipse.swordfish.core.test;
 import org.eclipse.swordfish.api.context.SwordfishContext;
 import org.eclipse.swordfish.core.test.util.OsgiSupport;
 import org.eclipse.swordfish.core.test.util.base.TargetPlatformOsgiTestCase;
+import org.osgi.framework.Bundle;
+import org.springframework.osgi.util.OsgiBundleUtils;
 
 /**
  * The central sentinel test that checks if platform has been started correctly. The test is launched after all bundles are started
@@ -11,6 +13,14 @@ import org.eclipse.swordfish.core.test.util.base.TargetPlatformOsgiTestCase;
  */
 public class CentralSentinelTest extends TargetPlatformOsgiTestCase {
     public void test1SwordfishHasStarted() {
+        for (Bundle bundle : bundleContext.getBundles()) {
+            if (OsgiBundleUtils.isFragment(bundle)) {
+                assertEquals(String.format("Fragment bundle [%s] should be in the INSTALLED state", bundle.getSymbolicName()), Bundle.INSTALLED, bundle.getState());
+            } else {
+                assertEquals(String.format("Bundle [%s] should be in the ACTIVE state", bundle.getSymbolicName()), Bundle.ACTIVE, bundle.getState());
+
+            }
+        }
         SwordfishContext swordfishContext = OsgiSupport.getReference(bundleContext, SwordfishContext.class);
         assertNotNull(swordfishContext);
         assertNotNull(swordfishContext.getConfigurationService());
